ca89a9942807f303001547a60b43de25b1eb1e09
Robert-Pon <66363857+Robert-Pon@users.noreply.github.com>
2022-10-22 22:46:38 +0200

    Add files via upload

---
+++ android/java/data/Data.java
@@ -0,0 +1,31 @@
+package com.name.social_helper_r_p.data;
+
+public class Data {
+    private String id = "";
+    private String value = "";
+    private int type = 0;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
